b60480e2b6123a4284a06ed4282adc56ff592a3f
Igor Loskutov <igor.loskutoff@gmail.com>
2013-08-14 09:52:19 +0400

    init application

--- topico-content-editors.src.js
+++ topico-content-editors.src.js
@@ -1,3 +1,8 @@
+angular.module('topicoContentEditorsApp', []);
+
+/*
+//@ sourceMappingURL=init.js.map
+*/
 'use strict';
 var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
